a5ecb5d1c0f463f91ff20b55a0dc20f57018b703
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-03-05 13:59:08 +0600

    trusted section added

--- src/app/(home)/home.tsx
+++ src/app/(home)/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import OurServices from './ourservices'
 import Stats from './stats'
+import Trusted from './trusted'
 
 export default function HomePage() {
   return (
     <div className=''>
       <Stats/>
     <OurServices/>
+    <Trusted/>
     </div>
   )
 }
---
+++ src/app/(home)/trusted.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Trusted() {
+  return (
+    <div>
+   <section className="py-12 bg-gray-100 text-center">
+        <h2 className="text-3xl font-semibold">Trusted By</h2>
+        <div className="flex flex-wrap justify-center items-center gap-16 mt-8 px-6">
+          <img src="/logo1.png" alt="Company 1" className="h-12"/>
+          <img src="/logo2.png" alt="Company 2" className="h-12"/>
+          <img src="/logo3.png" alt="Company 3" className="h-12"/>
+          <img src="/logo4.png" alt="Company 4" className="h-12"/>
+          <img src="/logo5.png" alt="Company 5" className="h-12"/>
+          <img src="/logo5.png" alt="Company 6" className="h-12"/>
+          <img src="/logo5.png" alt="Company 7" className="h-12"/>
+          <img src="/logo5.png" alt="Company 8" className="h-12"/>
+          <img src="/logo5.png" alt="Company 9" className="h-12"/>
+        </div>
+      </section>
+    </div>
+  )
+}
